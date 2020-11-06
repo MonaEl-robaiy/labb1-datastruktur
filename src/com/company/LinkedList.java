5a0f4bac54f3e99b82db32f4ba25368d662b99ce
Mona <mona.elrobaiy@student.kyh.se>
2020-11-06 15:47:03 +0100

    initial commit

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -1,51 +1,114 @@
 package com.company;
 
+
 public class LinkedList {
-
-
     private Node head;
+    private int listCount;
+    private int size;
 
-    public int size(){
-        if (head == null){
-            return 0;
-        }
-        else {
-            int i = 1;
-            Node n = head;
-            while(n.next != null ){
-                n = n.next;
-                i++;
-            }
-            return  i;
+    public class Node {
+        protected int value;
+        protected Node next;
+
+        public Node(int v) {
+            value = v;
         }
     }
-    public int get(int index){
-        int i = 0;
-        Node n = head;
-        while (i < index){
-            n = n.next;
-            i++;
-        }
-       return n.value;
-        }
 
-
-    public void add(int v) {
+    public void add(int e) {
+        Node newNode= new Node (e);
         if (head == null) {
-            head = new Node(v);
+            head = newNode;
         } else {
             Node n = head;
             while (n.next != null) {
                 n = n.next;
+
             }
+            n.next = newNode;
+
         }
     }
-    class Node{
-        protected int value;
-        protected Node next;
-    public Node(int v){
-        this.value = v;
+// inte klat
+    public void add(int i, int e) {
+        Node n = head;
+        head = new Node(e);
+        while (i < e) {
+            n = n.next;
+            i++;
+        }
+    }
+//inte klar
+    public boolean contains(Object o) {
+            String e = null;
+        while (o==null;e == null){
 
+            if(o==null? e == null : o.equals(e)){
+                return true;
+
+            }
+
+        }
+
+       /*
+        Node n=head;
+        if (n.equals(e)){
+            return true;
+        }
+        return false;*/
     }
+
+    public int get(int index) {
+        int i = 0;
+        Node n = head;
+        while (i < index) {
+            n = n.next;
+            i++;
+        }
+        return n.value;
     }
+
+     public void remove( int i) {
+        if (i==0){
+            head = head.next;
+            return;
+        }
+        Node n = head;
+        int j = 0;
+        while (j < i-1){
+            n=n.next;
+            j++;
+        }
+        n.next = n.next.next;
+        size --;
+    }
+
+    public void prettyprint(){
+        System.out.print("LinkedList ->");
+        Node n = head;
+        while (n != null){
+            System.out.print("Node(" + n.value + ") ->");
+            n = n.next;
+
+        }
+    }
+    public boolean empty() {
+        return (head == null);
+    }
+    public int size() {
+        if (head == null) {
+            return 0;
+        } else {
+            int i = 1;
+            Node n = head;
+            while (n.next != null) {
+                n = n.next;
+                i++;
+            }
+            return i;
+        }
+    }
+
 }
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,45 @@
 package com.company;
-import java.util.LinkedList;
+
 public class Main {
 
-
-
     public static void main(String[] args) {
-            LinkedList<Integer> list = new LinkedList<Integer>();
-            // lägg till 5,10,25
-            list.add(5);
-            list.add(10);
-            list.add(25);
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.prettyprint();
+      /*  System.out.println("LinkedList \n~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Current list: " + list);
+        System.out.println("List size before adding element: " + list.size()); // --> 3
 
-            System.out.println(list.get(0));
-            System.out.println(list.get(1));
-            System.out.println(list.get(2));
-        }
+        list.add(0,99);
 
+        System.out.println("List size after adding element: " + list.size()); // --> 4
+
+        System.out.println("Get index 0: " + list.get(0)); // --> 99
+        System.out.println("Get index 4: " + list.get(3)); // --> 3
+
+        list.remove(3);
+
+        System.out.println("List size after removing element: " + list.size()); // --> 3
+        System.out.println("Returns true if list contains element 3: " + list.contains(3)); // --> returns true
+        System.out.println("Returns false as list is not empty: " + list.isEmpty());
+
+        Stack <Integer> stack = new Stack<>();
+        System.out.println("\nStack\n~~~~~~~~~~~~~~~~~~~~~~~~~");
+        stack.push(1);
+        stack.push(10);
+        stack.push(100);
+        System.out.println(stack);
+        System.out.println("Peek displays the top element: " + stack.peek());
+        stack.pop();
+        System.out.println("We used pop to remove an element, new top element: " + stack.peek());
+
+
+
+
+
+        System.out.println(list.contains(null));*/
     }
 
+}
